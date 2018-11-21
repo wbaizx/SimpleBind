61989288cbe083c9c26ce664488d8c5b83085254
wb <524323451@qq.com>
2018-11-21 18:06:43 +0800

    no1

---
+++ app/src/main/java/com/simplebind/MainActivity.java
@@ -0,0 +1,19 @@
+package com.simplebind;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    @BindView
+    TextView test;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        SimpleBind.bind(this);
+        test.setText("绑定成功");
+    }
+}
